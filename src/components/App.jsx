d0a211eddabe2be5cc45b08cc39f71697808ad05
Mariia Makarkina <mariiamak25@gmail.com>
2023-01-26 19:28:36 +0200

    add didmount and didupgrade

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import styles from '../components/phoneBook.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -69,6 +64,21 @@ export class App extends Component {
     });
     return Boolean(contact);
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contacts && contacts.length > 0) {
+      this.setState({ contacts: contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      const { contacts } = this.state;
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   render() {
     const isContacts = Boolean(this.state.contacts.length);
